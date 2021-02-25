a324695781bb358360090387562c2f3a3946a2a1
Marko Skugor <marko.skugor.1@gmail.com>
2021-02-24 20:43:42 -0500

    keyboard event handling

--- src/stories/keyboard/Keyboard.js
+++ src/stories/keyboard/Keyboard.js
@@ -3,28 +3,65 @@ import PropTypes from 'prop-types';
 
 import './keyboard.scss';
 
+const KEY_MAP = {
+  a: 'c',
+  w: 'cSharp',
+  s: 'd',
+  e: 'dSharp',
+  d: 'e',
+  f: 'f',
+  t: 'fSharp',
+  g: 'g',
+  y: 'gSharp',
+  h: 'a',
+  u: 'aSharp',
+  j: 'b',
+  k: 'c',
+  o: 'cSharp',
+  l: 'd',
+  p: 'dSharp',
+};
+
 class Keyboard extends React.Component {
-  createKeyPressedCallback(octave, note) {
-    return () => {
-      this.props.onKeyPressed(octave, note)
-    };
+  constructor(props) {
+    super(props);
+
+    document.addEventListener('keydown', ({ key }) => {
+      if (KEY_MAP[key]) {
+        let octave = this.props.startingOctave;
+        if ('kolp'.indexOf(key) !== -1) octave += 1;
+        this.props.onKeyPressed(octave, KEY_MAP[key])
+        this.triggerKeyPressActiveState(key, octave);
+      }
+    });
+  }
+
+  triggerKeyPressActiveState(key, octave) {
+    const keyElement = document.getElementById(`${KEY_MAP[key]}${octave}`);
+
+    if (keyElement) {
+      keyElement.classList.add('active');
+      setTimeout(() => {
+        keyElement.classList.remove('active');
+      }, 100);
+    }
   }
 
   renderKeyboardOctave(octave) {
     return (
       <div key={octave} className='keyboard__octave'>
-        <button onClick={this.createKeyPressedCallback(octave, 'c')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'cSharp')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'd')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'dSharp')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'e')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'f')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'fSharp')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'g')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'gSharp')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'a')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'aSharp')}></button>
-        <button onClick={this.createKeyPressedCallback(octave, 'b')}></button>
+        <button id={`c${octave}`} onClick={() => this.props.onKeyPressed(octave, 'c')}></button>
+        <button id={`cSharp${octave}`} onClick={() => this.props.onKeyPressed(octave, 'cSharp')}></button>
+        <button id={`d${octave}`} onClick={() => this.props.onKeyPressed(octave, 'd')}></button>
+        <button id={`dSharp${octave}`} onClick={() => this.props.onKeyPressed(octave, 'dSharp')}></button>
+        <button id={`e${octave}`} onClick={() => this.props.onKeyPressed(octave, 'e')}></button>
+        <button id={`f${octave}`} onClick={() => this.props.onKeyPressed(octave, 'f')}></button>
+        <button id={`fSharp${octave}`} onClick={() => this.props.onKeyPressed(octave, 'fSharp')}></button>
+        <button id={`g${octave}`} onClick={() => this.props.onKeyPressed(octave, 'g')}></button>
+        <button id={`gSharp${octave}`} onClick={() => this.props.onKeyPressed(octave, 'gSharp')}></button>
+        <button id={`a${octave}`} onClick={() => this.props.onKeyPressed(octave, 'a')}></button>
+        <button id={`aSharp${octave}`} onClick={() => this.props.onKeyPressed(octave, 'aSharp')}></button>
+        <button id={`b${octave}`} onClick={() => this.props.onKeyPressed(octave, 'b')}></button>
       </div>
     );
   }
